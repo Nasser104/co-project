9cff2424d312c4c801725ee02553dbc832437e3c
Nasser104 <110768143+Nasser104@users.noreply.github.com>
2024-11-03 12:30:49 +0200

    Update searchpage.component.ts

--- src/app/searchpage/searchpage.component.ts
+++ src/app/searchpage/searchpage.component.ts
@@ -6,21 +6,15 @@ interface DisplayState {
   showAll: boolean;
   showRecent: boolean;
   showPosts: boolean;
-  showPhotos: boolean;
-  showVideos: boolean;
-  showProfiles: boolean;
+  showPhoto: boolean;
+  showVideo: boolean;
   showPages: boolean;
-  showCompanies: boolean;
+  showProfiles: boolean;
+  showComps: boolean;
   showNews: boolean;
+  showBCC: boolean,
   showPANDL: boolean;
-}
-
-interface FeedItem {
-  title: string;
-  contentSnippet: string;
-  link: string;
-  // Add other properties as needed
-}
+};
 
 @Component({
   selector: 'app-searchpage',
@@ -31,17 +25,17 @@ export class SearchpageComponent {
 
   // new code 
 
-  // تهيئة الحالة الافتراضية
   displayState: DisplayState = {
     showAll: true,
     showRecent: true,
     showPosts: false,
-    showPhotos: false,
-    showVideos: false,
+    showPhoto: false,
+    showVideo: false,
     showProfiles: false,
     showPages: false,
-    showCompanies: false,
+    showComps: false,
     showNews: false,
+    showBCC: false,
     showPANDL: false,
   };
 
@@ -51,74 +45,9 @@ export class SearchpageComponent {
     Object.keys(this.displayState).forEach((k) => {
       this.displayState[k as keyof DisplayState] = false;
     });
-    
+
     // تعيين القيمة المطلوبة إلى true
     this.displayState[key] = true;
-  }
-
-  // الكود القديم
-
-  inputValue: string = '';
-
-  showAll: boolean = true; 
-  showPosts: boolean = false; 
-  showPages: boolean = false; 
-  showImages: boolean = false; 
-  showVideos: boolean = false; 
-  showBlog: boolean = false; 
-
-  toggleAll(): void {
-    this.showAll = true;
-    this.showPosts = false;
-    this.showPages = false;
-    this.showImages = false;
-    this.showVideos = false;
-    this.showBlog = false;
-  }
-
-  togglePosts(): void {
-    this.showPosts = true;
-    this.showAll = false;
-    this.showPages = false;
-    this.showImages = false;
-    this.showVideos = false;
-    this.showBlog = false;
-  }
-
-  togglePages(): void {
-    this.showPages = true;
-    this.showAll = false;
-    this.showPosts = false;
-    this.showImages = false;
-    this.showVideos = false;
-    this.showBlog = false;
-  }
-
-  toggleImages(): void {
-    this.showImages = true;
-    this.showAll = false;
-    this.showPosts = false;
-    this.showPages = false;
-    this.showVideos = false;
-    this.showBlog = false;
-  }
-
-  toggleVideos(): void {
-    this.showVideos = true;
-    this.showAll = false;
-    this.showPosts = false;
-    this.showPages = false;
-    this.showImages = false;
-    this.showBlog = false;
-  }
-
-  toggleBlog(): void {
-    this.showBlog = true;
-    this.showAll = false;
-    this.showPosts = false;
-    this.showPages = false;
-    this.showImages = false;
-    this.showVideos = false;
   }
 
 ////////////////////////////////////////
